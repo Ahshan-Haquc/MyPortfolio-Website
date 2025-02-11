c1b14be11517e2140f4ecf97e7c465575e67adb2
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2025-02-11 15:43:34 -0800

    adding animation in skill page

---
+++ script.js
@@ -0,0 +1,8 @@
+function skillPageHover(divBox) {
+    const element = document.getElementById(divBox);
+    element.classList.add("upWhenHover");
+}
+function removeSkillPageHover(divBox) {
+    const element = document.getElementById(divBox);
+    element.classList.remove("upWhenHover");
+}
