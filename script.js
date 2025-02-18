722ce9bbfee5f401d0433a9c9061a8f1afeaf237
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2025-02-18 01:22:55 -0800

    starting making responsive with footer, navbar, project and career page

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function removeSkillPageHover(divBox) {
     const element = document.getElementById(divBox);
     element.classList.remove("upWhenHover");
 }
+
+
+function toggleMenu() {
+    let menu = document.getElementById("menuDiv");
+    let menuIcon = document.getElementById("menuIcon");
+
+    if (menu.classList.contains("left-[-400px]")) {
+      menu.classList.remove("left-[-400px]");
+      menu.classList.add("left-0");
+      menuIcon.classList.add("rotate-180");
+    } else {
+      menu.classList.remove("left-0");
+      menu.classList.add("left-[-400px]");
+      menuIcon.classList.remove("rotate-180");
+    }
+  }
